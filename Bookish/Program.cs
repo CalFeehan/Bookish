1b54d3995725b65d9c8854eb70faf749870627a3
Ian Nkwocha <11nkwochaian@gmail.com>
2021-12-16 12:15:00 +0000

    Added display for available copies of book Added some styling

--- Bookish/Program.cs
+++ Bookish/Program.cs
@@ -30,30 +30,29 @@ namespace Bookish.ConsoleApp
                 Console.WriteLine(new string('*', 20));
             }
 
-            foreach (Checkout checkout in CheckoutRepo.GetUserCheckouts("callum.feehan@iris.co.uk"))
+            foreach (Checkout checkout in CheckoutRepo.GetUserCheckouts("ian.nkwocha@iris.co.uk"))
             {
                 Console.WriteLine(checkout);
             }
 
-            // Run CSV import
-            // CSVReader.AddCSVLineToBookDatabase(CSVReader.ExtractFromCSV(@"C:\Users\User\Desktop\amended_books.csv", true));
-           
-            // Checkout first 23 books
+            //Run CSV import
+            //CSVReader.AddCSVLineToBookDatabase(CSVReader.ExtractFromCSV(@"C:\Users\Ian.Nkwocha\Downloads\amended_books.csv", true));
+            //Checkout first 23 books
             //for (int i = 7; i < 30; i++)
             //{ CheckoutRepo.AddCheckout("callum.feehan@iris.co.uk", i); }
-
-            // CheckoutRepo.AddCheckout("test@test.com", 1);
+            //
+            //CheckoutRepo.AddCheckout("test@test.com", 1);
             //CheckoutRepo.AddCheckout("danny.flahive@iris.co.uk", 1);
             //CheckoutRepo.AddCheckout("danny.flahive@iris.co.uk", 8);
             //CheckoutRepo.AddCheckout("ian.nkwocha@iris.co.uk", 1);
-            //CheckoutRepo.AddCheckout("ian.nkwocha@iris.co.uk", 9);
+            //CheckoutRepo.AddCheckout("ian.nkwocha@iris.co.uk", 89);
             //CheckoutRepo.AddCheckout("callum.feehan@iris.co.uk", 1);
-
+            //
             //foreach (Checkout checkout in CheckoutRepo.GetUserCheckouts("callum.feehan@iris.co.uk"))
             //{
             //    Console.WriteLine(checkout);
             //}
-
+            
         }
     }
 }

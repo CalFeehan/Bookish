2147707973dad9ce7054f1310493b3818d889f95
Callum Feehan <72271107+CalFeehan@users.noreply.github.com>
2021-12-20 13:06:36 +0000

    Revert "update"
    This reverts commit 87ae16b95628a43fed2de1ef74c57060dad43d7f.

--- Bookish.MVCWeb/Startup.cs
+++ Bookish.MVCWeb/Startup.cs
@@ -51,9 +51,9 @@ namespace Bookish.MVCWeb
             {
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                //app.UseHsts();
+                app.UseHsts();
             }
-            //app.UseHttpsRedirection();
+            app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
